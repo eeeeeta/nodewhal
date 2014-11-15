4834845d03d997a003220162bccc83f8c95ee718
eeeeeta <eeeeeta@users.noreply.github.com>
2014-11-15 10:27:47 +0000

    strip it bare, and leave only the vaguely good parts

--- nodewhal.js
+++ nodewhal.js
@@ -6,7 +6,7 @@ var request = require('request'),
   knownShadowbans = {},
   lastRedditRequestTimeByUrl = {},
   lastRedditRequestTime,
-  defaultUserAgent = 'noob-nodewhal-dev-soon-to-be-ip-banned';
+  defaultUserAgent = 'unspecified user agent';
 
 function NodewhalSession(userAgent) {
   var self = this;
@@ -62,239 +62,6 @@ function NodewhalSession(userAgent) {
     });
   };
 
-  self.comment = function (thing_id, markdown) {
-    return self.post(baseUrl + '/api/comment', {
-      form: {
-        api_type: 'json',
-        text: markdown,
-        thing_id: thing_id,
-        uh: self.session.modhash
-      }
-    });
-  };
-
-  self.flair = function (subreddit, linkName, template, flairText) {
-    return self.post(baseUrl + '/api/flair', {
-      form: {
-        api_type: 'json',
-        link: linkName,
-        r: subreddit,
-        text: flairText,
-        css_class: template,
-        uh: self.session.modhash
-      }
-    })
-  };
-
-  self.aboutUser = function (username) {
-    return self.get(
-        baseUrl + '/user/' + username + '/about.json'
-      ).then(function (json) {
-        return json.data;
-      });
-  };
-
-  self.submitted = function (subreddit, url) {
-    url = encodeURIComponent(url);
-    return self.get(baseUrl + '/r/' + subreddit + '/submit.json?url=' + url, {});
-  };
-
-  self.moderated = function() {
-    return self.get(baseUrl + '/subreddits/mine/moderator.json').then(function(json) {
-      return json.data.children.map(function(child) {return child.data;});
-    });
-  };
-
-  self.modlog = function(name) {
-    return self.get(baseUrl + '/r/' + name + '/about/log.json').then(function(json) {
-      return json.data.children.map(function(child) {return child.data;});
-    });
-  };
-
-  self.duplicates = function (subreddit, id) {
-    if (id.indexOf('_') !== -1) {
-      id = id.split('_')[1];
-    }
-    return self.get(baseUrl + '/r/' + subreddit + '/duplicates/' + id + '/_/.json');
-  };
-
-  self.checkForShadowban = function (username) {
-    var url = baseUrl + '/user/' + username;
-    return new RSVP.Promise(function (resolve, reject) {
-      if (knownShadowbans[username]) {
-        return reject('shadowban');
-      }
-      Nodewhal.respectRateLimits('get', url).then(function () {
-        request(url, {}, function (error, response, body) {
-          if (error) {
-            reject(error);
-          } else {
-            if (body.indexOf('the page you requested does not exist') === -1) {
-              resolve(username);
-            } else {
-              knownShadowbans[username] = true;
-              reject('shadowban');
-            }
-          }
-        });
-      });
-    });
-  };
-
-  self.listing = function (listingPath, options) {
-    var url = baseUrl + listingPath + '.json',
-      options = options || {},
-      max = options.max,
-      after = options.after,
-      limit = max || 100;
-    if (limit > 100) {limit = 100;}
-    if (url.indexOf('?') < 0) {
-      url += '?limit=' + limit;
-    } else {
-      url += '&limit=' + limit;
-    }
-    if (after) {
-      url += '&after=' + after;
-    }
-    return self.get(url, {}).then(function (listing) {
-      var results = {}, resultsLength;
-      if (listing && listing.data && listing.data.children && listing.data.children.length) {
-        listing.data.children.forEach(function (submission) {
-          resultsLength = Object.keys(results).length;
-          if (!max || resultsLength < max) {
-            results[submission.data.name] = submission.data;
-          }
-        });
-        //console.log("Length:", resultsLength);
-
-        if (
-          listing.data.after &&
-            (typeof max === 'undefined' || resultsLength + 1 < max)
-          ) {
-          if (!typeof max === 'undefined') {
-            max = max - resultsLength;
-          }
-          return schedule.wait(options.wait).then(function () {
-            return self.listing(listingPath, {
-              max: max,
-              after: listing.data.after,
-              wait: options.wait
-            }).then(function (moreResults) {
-                Object.keys(moreResults).forEach(function (key) {
-                  results[key] = moreResults[key];
-                });
-                return results;
-              })
-          });
-        } else {
-          return results;
-        }
-      } else {
-        return {};
-      }
-    });
-  };
-
-  self.startSubmissionStream = function (cb, subreddit, author, domain, is_self) {
-    url = "http://api.rednit.com/submission_stream?eventsource=true";
-    if (subreddit && subreddit.length) {
-      subreddit = subreddit.join('+');
-    }
-    if (subreddit) {
-      url += "&subreddit=" + subreddit;
-    }
-    if (author) {
-      url += "&author=" + author;
-    }
-    if (domain) {
-      url += "&domain=" + domain;
-    }
-    if (is_self) {
-      url += "&is_self=" + is_self;
-    }
-    console.log('stream start:', url);
-    self.es = new EventSource(url);
-    if (cb != null) {
-      self.es.onmessage = function (e) {
-        cb(JSON.parse(e.data));
-      }
-    }
-    else {
-
-      self.es.onmessage = function (e) {
-        self.newSubmissions.push(JSON.parse(e.data));
-      };
-      self.es.onerror = function () {
-        console.log("Error in the submission stream.");
-      }
-    }
-  };
-
-  self.stopSubmissionStream = function () {
-    self.es.close();
-  };
-
-  self.byId = function (ids) {
-    var isSingle = false;
-    if (typeof ids == "string") {
-      ids = [ids];
-      isSingle = true;
-    }
-    ids = ids.map(function (id) {
-      if (id.substr(0, 3) == "t3_") {
-        return id
-      }
-      else {
-        return "t3_" + id;
-      }
-    });
-
-    var fetch_ids_wrapper = function (u) {
-      var url = u;
-      return function () {
-        return self.get(url, {}).then(function (listing) {
-          var results = {};
-          if (listing && listing.data && listing.data.children && listing.data.children.length) {
-            if (isSingle) {
-              return listing.data.children[0].data;
-            }
-            listing.data.children.forEach(function (submission) {
-              results[submission.data.name] = submission.data;
-            });
-          }
-          return results;
-        });
-      };
-    };
-    if (ids.length <= 100) {
-      var url = baseUrl + "/by_id/" + ids.join(",") + '/.json';
-      return fetch_ids_wrapper(url)();
-    }
-    else {
-      var promises = [];
-
-      for (var i = 0; i < (ids.length + 100); i += 100) {
-        if (ids.slice(i, i + 101).length > 0) {
-          u = baseUrl + "/by_id/" + ids.slice(i, i + 101).join(",") + '/.json?limit=100';
-          promises.push(fetch_ids_wrapper(u))
-        }
-      }
-      return schedule.runInSeries(promises).then(function (resultList) {
-        var results = {};
-        var len = 0;
-        resultList.forEach(function (element, index, array) {
-          for (attrname in element) {
-            results[attrname] = element[attrname];
-          }
-        });
-
-        return results;
-      });
-
-    }
-
-  };
-
   self.get = function (url, opts) {
     return self.req(url, 'get', opts);
   };
@@ -317,16 +84,13 @@ function NodewhalSession(userAgent) {
         try {
           json = JSON.parse(body);
         } catch (e) {
-          console.error('Cant parse', url, method, opts,  body);
           throw e;
         }
         if (json && json.error) {
-          console.log('error', json);
           throw Error(json.error);
         }
         return json;
       }, function (error) {
-        console.error(error.stack || error);
         throw error;
       });
   };
@@ -340,11 +104,6 @@ Nodewhal.schedule = schedule;
 
 Nodewhal.rsvpRequest = function (method, url, opts) {
   return new RSVP.Promise(function (resolve, reject) {
-    if (url.indexOf('api/login') === -1 && method === 'post') {
-      console.log(method, url, JSON.stringify(opts.form));
-    } else {
-      console.log(method, url);
-    }
     if (!method || method === 'get') {
       method = request;
     } else {
